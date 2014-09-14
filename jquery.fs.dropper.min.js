5f07a9e96672488bd348c1cf8a4d4cd4f0888c2a
Ben Plum <mr@benplum.com>
2014-09-14 11:05:52 -0400

    Fixing issue with demo

--- jquery.fs.dropper.min.js
+++ jquery.fs.dropper.min.js
@@ -1,5 +1,5 @@
 /* 
- * Dropper v0.1.2 - 2014-09-13 
+ * Dropper v0.1.3 - 2014-09-14 
  * A jQuery plugin for simple drag and drop uploads. Part of the Formstone Library. 
  * http://formstone.it/dropper/ 
  * 
